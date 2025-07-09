6086a2fe539f129b25bc0e64ae3cec0ead041fa1
Airdrop Legion Community <kodhamwalang@gmail.com>
2025-07-09 11:15:28 +0700

    Create banner.js

---
+++ config/banner.js
@@ -0,0 +1,9 @@
+const banner = [
+    '\n======================================', 
+    '\n    PHAROS AUTO BOT BY AIRDROP ALC    ',  
+    '\n======================================',                                                                                                                                                                                                                                
+]
+
+module.exports = {
+    banner
+};
